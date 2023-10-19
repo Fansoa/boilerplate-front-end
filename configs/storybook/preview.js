3ed43cc97a51f4ce5a5c62ad724d1beba9d9a8b8
Francois Verin <francois@tekyn.com>
2023-10-19 15:47:14 +0200

    ✨: init storybook and add theming of emotion

---
+++ configs/storybook/preview.js
@@ -0,0 +1,26 @@
+import { withThemeFromJSXProvider } from "@storybook/addon-themes";
+import { ThemeProvider } from "@emotion/react";
+import theme from "../../src/theme";
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    themes: { default: theme },
+    defaultTheme: "default",
+    Provider: ThemeProvider,
+  }),
+];
+
+export default preview;
