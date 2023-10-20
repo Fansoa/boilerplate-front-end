7c46fc948b2ef964367dffbd60beedd5f7a1307b
Francois Verin <francois@tekyn.com>
2023-10-20 16:36:17 +0200

    ✨(TestComponent): update to get more cases to test

--- src/components/TestComponent/index.jsx
+++ src/components/TestComponent/index.jsx
@@ -1,8 +1,23 @@
-import { Container } from "./styled";
 import PropTypes from "prop-types";
+import { useState } from "react";
+import { Container } from "./styled";
 
 const TestComponent = ({ variant }) => {
-  return <Container variant={variant}>TestComponent</Container>;
+  const [isClicked, setIsClicked] = useState(false);
+
+  return (
+    <Container variant={variant}>
+      <p>TestComponent</p>
+      <button
+        onClick={() => {
+          setIsClicked((curr) => !curr);
+        }}
+      >
+        Bouton
+      </button>
+      {isClicked && <p>isClicked</p>}
+    </Container>
+  );
 };
 
 TestComponent.propTypes = {
